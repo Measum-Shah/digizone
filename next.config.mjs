4f472bf2e8f0cc6c64cadd0c8dd838e434ef98ac
Measum-Shah <shahmeasum008@gmail.com>
2025-11-30 02:45:16 +0500

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-    
-    // Optional: If you use images, you might need to disable the Image Optimization 
-    // loader if you are deploying a pure static site without a separate image server.
-    images: {
-        unoptimized: true,
-    },
+  
 };
 
 export default nextConfig;
